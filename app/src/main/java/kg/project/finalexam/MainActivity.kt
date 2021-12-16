9b9354a3e33c5b191fba3490c34a5092e582aea7
tatarrrrrr <darknight0417@gmail.com>
2021-12-16 15:09:32 +0600

    ExamFinal

---
+++ app/src/main/java/kg/project/finalexam/MainActivity.kt
@@ -0,0 +1,13 @@
+package kg.project.finalexam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_main2.xml`)
+        val exit Button= findViewById<Button>
+
+    }
+}
